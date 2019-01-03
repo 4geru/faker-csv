2737e21ed61795e486d3df2929fd5e5d423b761c
Rp7rf <westhouse51@gmail.com>
2019-01-03 22:59:59 +0900

    add commands

--- lib/faker/csv.rb
+++ lib/faker/csv.rb
@@ -1,8 +1,29 @@
-require "faker/csv/version"
+require 'faker/csv/version'
+require 'faker/csv/maker'
+require 'json'
+require 'faker'
 
 module Faker
   module Csv
-    class Error < StandardError; end
-    # Your code goes here...
+    def self.make(file_path, output_file_path = nil)
+      # Faker::Config.locale = :ja
+
+      p "called make #{file_path}"
+      json = json_parse(file_path)
+      exit unless json
+      Maker.new(json).make
+    end
+
+    private
+
+    def self.json_parse(file_path)
+      json = open(file_path) do |io|
+        JSON.load(io)
+      end
+      json
+    rescue Errno::ENOENT => e
+      puts 'JSON file not found'
+      return false
+    end
   end
 end
---
+++ lib/faker/csv/maker.rb
@@ -0,0 +1,59 @@
+
+module Faker
+  module Csv
+    class Maker
+      def initialize(json)
+        @json = json
+      end
+
+      def make
+        print make_titles + make_content
+      end
+
+      private
+
+      def make_titles
+        @json.keys.join(', ') + "\n"
+      end
+
+      def make_content
+        set_variables
+
+        (0...@indexs[:count]).map do |count|
+          make_row(count)
+        end.join("\n") + "\n"
+      end
+
+      def make_row(count)
+        row = []
+        @json.each do |key, value|
+          if value.kind_of?(Array)
+            index = count / @indexs[key][:prev] % @indexs[key][:index]
+            row << value[index]
+          elsif value == 'index'
+            row << count + 1
+          elsif value =~ /Faker/
+            row << eval(value)
+          elsif value =~ /rb/
+          else
+            row << value
+          end
+        end
+        row.join(', ')
+      end
+
+      def set_variables
+        @indexs = {}
+        index = 1
+        @json.each do |key, value|
+          next unless value.kind_of?(Array)
+          @indexs[key] = {prev: index, index: value.count}
+          index = index * value.count
+        end
+
+        @indexs[:count] = index
+        true
+      end
+    end
+  end
+end
