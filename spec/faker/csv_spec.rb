9f4fb9ed6389d6bb79baefea160a41f7b51f587b
Rp7rf <westhouse51@gmail.com>
2019-01-06 01:32:18 +0900

    add .circieci/config.yml

--- spec/faker/csv_spec.rb
+++ spec/faker/csv_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Faker::Csv do
   it "has a version number" do
     expect(Faker::Csv::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
