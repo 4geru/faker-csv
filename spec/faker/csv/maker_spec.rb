2c71792671b51106be1b41a2617d0daba13f6b04
Rp7rf <westhouse51@gmail.com>
2019-01-06 13:24:56 +0900

    add rspec

--- spec/faker/csv/maker_spec.rb
+++ spec/faker/csv/maker_spec.rb
@@ -1,24 +1,38 @@
 RSpec.describe Faker::Csv::Maker do
   describe 'Instance Method' do
     context '#make' do
-      context 'normal' do
-        subject { described_class.new({cat: 'dog'}).make }
-        it do
-          is_expected.to eq("cat\ndog\n")
-        end
-      end
-
-      context 'multiple patterns' do
+      context 'array patterns' do
         subject { described_class.new({cat: ['dog', 'pig']}).make }
         it do
           is_expected.to eq("cat\ndog\npig\n")
         end
       end
 
+      context 'index' do
+        subject { described_class.new({cat: 'index'}).make }
+        it do
+          is_expected.to eq("cat\n1\n")
+        end
+      end
+
       context 'call ruby method' do
+        subject { described_class.new({cat: 'Faker::Name.name'}).make }
+        it do
+          is_expected.to match(/cat\n.*\n/)
+        end
+      end
+
+      context 'faker method' do
         subject { described_class.new({cat: 'rb 1 + 1'}).make }
         it do
           is_expected.to eq("cat\n2\n")
+        end
+      end
+
+      context 'normal' do
+        subject { described_class.new({cat: 'dog'}).make }
+        it do
+          is_expected.to eq("cat\ndog\n")
         end
       end
     end
